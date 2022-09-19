7207bf71df7ec4a16c22ba4be790249d10f5765d
dGarcia-NC <anthonygarcia833@gmail.com>
2022-09-19 16:21:37 -0700

    Reconfigure for AWS

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const { Client } = require('pg');
 
+
 const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
-  password: 'secret',
-  database: 'samwise-qa-db'
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 })
 
 module.exports = client;
